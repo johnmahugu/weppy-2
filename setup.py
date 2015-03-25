b4579d55ef50179673530b88e9aa2d08df6bbfa4
Rodrigo A. Lima <rodrigoalima99@gmail.com>
2015-03-24 23:17:11 -0300

    Downgrade Webob to version 1.2.3 for Google App Engine compatibility.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=['weppy'],
     package_dir={'': 'src'},
-    install_requires=['WebOb==1.3.1'],
+    install_requires=['WebOb==1.2.3'],
     author='Rodrigo A. Lima',
     description='A lightweight web framework that encourages clean design.',
     license='BSD',
