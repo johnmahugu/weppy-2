e3a53b577dee223b7535c47968c8c409ebf29781
Rodrigo A. Lima <rodrigoalima99@gmail.com>
2015-03-24 23:20:33 -0300

    Remove unnecessary code and imports from main.py.

--- tests/integration_tests/src/main.py
+++ tests/integration_tests/src/main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import sys
 from weppy.wsgi import *
 from settings import DEBUG, CONTROLLERS
 
-def application(environ, start_response):
-    return WSGIApplication(DEBUG, CONTROLLERS)(environ, start_response)
+application = WSGIApplication(DEBUG, CONTROLLERS)
